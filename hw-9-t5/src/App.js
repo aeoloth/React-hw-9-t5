67bbb5a64231f0b592c0728c7008564908cd2082
Eldar Zainashev <aeoloth@yandex.ru>
2021-09-23 20:46:23 +0300

    commit light version

--- hw-9-t5/src/App.js
+++ hw-9-t5/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
-import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
 import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Nav";
 import News from "./components/News/News";
-import Profile from "./components/Profile/Profile";
 import Settings from "./components/Settings/Settings";
+import { withSuspense } from "./withSuspense";
+// import Profile from "./components/Profile/Profile";
+// import Dialogs from "./components/Dialogs/Dialogs";
+
+const Profile = React.lazy(() => import('./components/Profile/Profile'));
+const Dialogs = React.lazy(() => import('./components/Dialogs/Dialogs'));
 
 const App = () => {
   return (
@@ -16,8 +20,10 @@ const App = () => {
         <Header />
         <Navbar />
         <div className="content">
-          <Route path='/profile' component={Profile} />
-          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/profile' 
+          render={withSuspense(Profile)}
+          />
+          <Route path='/dialogs' render={withSuspense(Dialogs)}/>
           <Route path='/news' component={News} />
           <Route path='/music' component={Music} />
           <Route path='/settings' component={Settings} />
---
+++ hw-9-t5/src/withSuspense.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const withSuspense = (Component) => {
+    return (props) => {
+        return <React.Suspense fallback={"Loading..."}><Component {...props} /></React.Suspense>
+    };
+}
